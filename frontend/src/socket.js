b6d359d8e839e3d2448c9dd0366dbe21d9501e1f
va1an <valan95@gmail.com>
2026-03-26 11:14:13 +0530

    socket bug fixes

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://chat-flow-k5cn.onrender.com:8080";
+const URL = "https://chat-flow-k5cn.onrender.com";
 
 export const socket = io(URL, {
     withCredentials: true,
